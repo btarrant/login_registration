c204d0f02f06f34931a0240ee0f03d6ca319111b
Brandon Tarrant <brandontarrant04@gmail.com>
2022-02-19 16:40:50 -0500

    Finalized the project.

--- flask_app/models/user.py
+++ flask_app/models/user.py
@@ -6,7 +6,7 @@ EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 
 class User:
-    db = "login_reg"
+    db = "login_and_registration"
 
     def __init__(self, data):
         self.id = data['id']
